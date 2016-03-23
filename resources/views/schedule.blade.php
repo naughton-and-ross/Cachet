d354c46e5f581351cc6709eab3097111f9a2099b
James Brooks <james@bluebaytravel.co.uk>
2016-03-23 18:04:12 +0000

    Show affected components in schedule page

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -1,27 +1,25 @@
 @extends('layout.master')
 
+@section('bodyClass', 'no-padding')
+
+@section('outer-content')
+@include('partials.nav')
+@stop
+
 @section('content')
-<div class="timeline">
-    <div class="content-wrapper">
-        <div class="moment first">
-            <div class="row event clearfix">
-                <div class="col-sm-1">
-                    <div class="status-icon status-{{ $schedule->status }}" data-toggle="tooltip" title="{{ $schedule->human_status }}" data-placement="left">
-                        <i class="icon ion-android-calendar"></i>
-                    </div>
-                </div>
-                <div class="col-xs-10 col-xs-offset-2 col-sm-11 col-sm-offset-0">
-                    <div class="panel panel-message incident">
-                        <div class="panel-heading">
-                            <strong>{{ $schedule->name }}</strong>, scheduled at {{ formatted_date($schedule->scheduled_at) }}
-                        </div>
-                        <div class="panel-body">
-                            {!! $schedule->formattedMessage !!}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<h1>{{ $schedule->name }} <small>{{ formatted_date($schedule->scheduled_at) }}</small></h1>
+
+<hr>
+
+<div class="markdown-body">
+    {!! $schedule->formattedMessage !!}
 </div>
+
+@if($schedule->components)
+<ul class="list-group">
+    @foreach ($schedule->components as $component)
+    <li class="list-group-item">{{ $component->name }}</li>
+    @endforeach
+</ul>
+@endif
 @stop
